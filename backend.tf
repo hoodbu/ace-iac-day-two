2110c55b7e55ed4c11ca6a2201e27c6de262cbaf
emereeclaire <57374616+emereeclaire@users.noreply.github.com>
2022-01-06 11:30:39 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "emeree_aviatrix"
     workspaces {
       name = "ace-iac-day-two"
     }
