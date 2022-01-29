2361ecefa0d9e57263b76784fd0abcbe860bc750
patch-ace-iac-netopss <98611931+patch-ace-iac-netopss@users.noreply.github.com>
2022-01-28 19:06:23 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    hostname = "app.terraform.io"
+    organization = "PGS-TPATCH"
     workspaces {
       name = "ace-iac-day-two"
     }
