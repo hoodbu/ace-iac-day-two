a4d839afd6c3005cae4aaa647fb266daf37659fb
TimmyBills1 <99155111+TimmyBills1@users.noreply.github.com>
2022-02-08 03:34:09 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "TimmyBills"
+    organization = "TimmyBills"
     workspaces {
       name = "ace-iac-day-two"
     }
