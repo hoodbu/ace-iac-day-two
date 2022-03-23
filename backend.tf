1ce7e5337fbeef876530d00783488ab0c2f2f40b
nikhildevl <101570734+nikhildevl@users.noreply.github.com>
2022-03-23 12:59:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "<ace-iac-day-two>"
+    # organization = "nikhil3004"
     workspaces {
       name = "ace-iac-day-two"
     }
