73217b2f026e26f1ffd7b278f915e9fd830f5905
anas-devops <102306594+anas-devops@users.noreply.github.com>
2022-03-24 18:35:39 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-     organization = "anas-netops"
+    organization = "anas-netops"
     workspaces {
       name = "ace-iac-daytwo"
     }
