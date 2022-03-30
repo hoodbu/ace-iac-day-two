4973626b5e064962c6d7ccb95073c4784bfbc163
nikhil3004 <41048498+nikhil3004@users.noreply.github.com>
2022-03-30 10:53:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    # organization = "<DRDO>"
     workspaces {
       name = "ace-iac-day-two"
     }
