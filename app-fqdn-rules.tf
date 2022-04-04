f2ac5266259ab9c1f3d764ac79e1072e342ad3c2
Max1211 <max.schmidt2@gmx.de>
2022-04-04 12:53:41 +0200

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -6,6 +6,7 @@ locals {
       "aviatrix.com"   = "80"
       "*.cisco.com"    = "80"
       "*.ubuntu.com"   = "80"
+      "*.heise.de"     = "443"
     }
     udp = {
       "dns.google.com" = "53"
