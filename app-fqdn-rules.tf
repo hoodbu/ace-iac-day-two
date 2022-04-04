e6af546e07d1777aee1dccf95fe235ef4c702c37
Max1211 <max.schmidt2@gmx.de>
2022-04-04 10:21:02 +0200

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -2,9 +2,9 @@
 locals {
   egress_rules = {
     tcp = {
-      "*.aviatrix.com" = "443"
-      "aviatrix.com"   = "80"
-      "computerbase.com"   = "80"
+      "*.aviatrix.com"     = "443"
+      "aviatrix.com"       = "80"
+      "*.computerbase.com"   = "80"
     }
     udp = {
       "dns.google.com" = "53"
