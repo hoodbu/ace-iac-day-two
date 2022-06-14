738e2ec1bdaa311f197742c59e08e1aeb23a9341
GPA-ACE-DEVOPS <107480904+GPA-ACE-DEVOPS@users.noreply.github.com>
2022-06-14 15:37:42 +0200

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -5,6 +5,7 @@ locals {
       "*.aviatrix.com" = "443"
       "aviatrix.com"   = "80"
       "*.ubuntu.com"   = "80"
+      "*.debian.com"   = "80"
     }
     udp = {
       "dns.google.com" = "53"
