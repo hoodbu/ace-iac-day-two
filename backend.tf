93d61f0f90c6712c6eeb4034df9197dd0999b326
NetOps-javi1r <114705928+NetOps-javi1r@users.noreply.github.com>
2022-09-30 18:50:40 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "-javi1r-sandbox"
+    organization = "javi1r"
     workspaces {
       name = "ace-iac-day-two"
     }
